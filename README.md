8e00b81c8735e47d3ba865d4174f8fad5621d2ba
Harsh Trivedi <harsh98trivedi@gmail.com>
2019-04-11 22:27:18 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# [Instagram Photo Downloader](https://harsh98trivedi.github.io/Instagram-Photo-Downloader) :fire:
+
+Free, Clean & Simple **Instagram Photo Downloader** for you. :slightly_smiling_face: <br>
+Download **Instagram Photos** in high quality with ease.<br>
+
+---
+
+![Instagram Photo Downloader](https://raw.github.com/harsh98trivedi/Instagram-Photo-Downloader/master/images/meta.jpg)
+
+#### Libraries Used
+
+---
+
+* Bootstrap - http://getbootstrap.com/
+* jQuery - http://jquery.com/
+
+> License: [GNU General Public License v3.0](https://github.com/harsh98trivedi/YouTube-Thumbnail-Downloader/blob/master/LICENSE.md)
+
+![Instagram Photo Downloader](https://img.shields.io/badge/Instagram-Photo%20Downloader-purple.svg)
---
+++ js/instaimage.js
@@ -0,0 +1,28 @@
+function insta_function(url) {
+    regExp = /((http:\/\/(instagram\.com\/p\/.*\/|www\.instagram\.com\/p\/.*\/))|(https:\/\/(www\.instagram\.com\/p\/.*\/)))/i;
+    match = url.match(regExp);
+    if (match && match[1].length >= 11) {
+        posturl = match[1];
+        imgpreview = posturl + 'media?size=m';
+        imagehq = posturl + 'media?size=l';
+        document.getElementById('imgpreview').src = imgpreview;
+        document.getElementById('imagehq').href = imagehq;
+    } else {
+        alert("The URL you have entered maybe incorrect. Please Enter a correct URL.");
+        location.reload();
+    }
+}
+$(document).keypress(function (event) {
+    if (event.which == 115 && (event.ctrlKey || event.metaKey) || (event.which == 19)) {
+        event.preventDefault();
+        return false;
+    }
+    return true;
+});
+
+$("#download-buttons").hide();
+$("#imagedloadbtn").click(function () {
+    $("#download-buttons").slideDown(500).fadeIn(250);
+    $("#imagedloadbtn").hide();
+    $(".input-group").hide();
+});
